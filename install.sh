2b19d0ba07af259f49de5dd6d4747c9c8c2a1d1f
Vincent Fages Gouyou <vfagesgo@gmail.com>
2026-05-11 12:22:15 +0200

    dd

--- install.sh
+++ install.sh
@@ -336,9 +336,9 @@ if [ 1 == 2 ]; then #VFG Debug
         "$CONF_FILE"
   }
   log_step "JAVA Config: Change -Xmx in VDBConfiguration.properties"
-  change_config "-Xmx" "/opt/denodo-9/conf/vdp/VDBConfiguration.properties" "2048m"
+  change_config "-Xmx" "/opt/denodo-9/conf/vdp/VDBConfiguration.properties" "3072m"
   log_step "JAVA Config: Change -XX:ReservedCodeCacheSize= in VDBConfiguration.properties"
-  change_config "-XX:ReservedCodeCacheSize=" "/opt/denodo-9/resources/apache-tomcat/conf/tomcat.properties" "128m"
+  change_config "-XX:ReservedCodeCacheSize=" "/opt/denodo-9/resources/apache-tomcat/conf/tomcat.properties" "256m"
   log_step "JAVA Config: Change -Xmx in resources/apache-tomcat/conf/tomcat.properties"
   change_config "-Xmx" "/opt/denodo-9/resources/apache-tomcat/conf/tomcat.properties" "1024m"
   
